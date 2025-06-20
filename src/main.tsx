69539b47340282f677c036fd9a915a2826cded1b
hikmagitz <hikmamif@gmail.com>
2025-06-20 02:59:00 +0200

    TransactionContext avec authentification basique

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { TransactionProvider } from './context/TransactionContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TransactionProvider>
+      <App />
+    </TransactionProvider>
   </StrictMode>
 );
