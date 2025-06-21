0e2ea29a72160db2bcfbfd7701ff7337c62b6cec
hikmagitz <hikmamif@gmail.com>
2025-06-21 18:45:05 +0200

    Make Premium Button Text Always Black

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -246,7 +246,7 @@ const Header: React.FC = () => {
                 <Button 
                   type="secondary"
                   onClick={() => navigate('/premium')}
-                  className="flex-1 bg-gradient-to-r from-yellow-100 to-orange-100 text-gray-900 hover:from-yellow-200 hover:to-orange-200 border-0 font-bold"
+                  className="flex-1 bg-gradient-to-r from-yellow-100 to-orange-100 text-black hover:from-yellow-200 hover:to-orange-200 border-0 font-bold"
                 >
                   <Crown size={16} />
                   {intl.formatMessage({ id: 'premium.upgrade' })}
@@ -336,7 +336,7 @@ const Header: React.FC = () => {
                 <Button 
                   type="secondary"
                   onClick={() => navigate('/premium')}
-                  className="bg-gradient-to-r from-yellow-100 to-orange-100 text-gray-900 hover:from-yellow-200 hover:to-orange-200 border-0 font-bold"
+                  className="bg-gradient-to-r from-yellow-100 to-orange-100 text-black hover:from-yellow-200 hover:to-orange-200 border-0 font-bold"
                 >
                   <Crown size={18} />
                   {intl.formatMessage({ id: 'premium.upgrade' })}
